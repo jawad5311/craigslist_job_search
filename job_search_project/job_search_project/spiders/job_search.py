dfa2481c6e5ea2d6bc542e779c1c3fdab1c86076
Jawad <muhammadjawad5311@gmail.com>
2021-12-15 12:28:37 +0500

    search response recieved successfully

--- job_search_project/job_search_project/spiders/job_search.py
+++ job_search_project/job_search_project/spiders/job_search.py
@@ -7,4 +7,18 @@ class JobSearchSpider(scrapy.Spider):
     start_urls = ['https://www.craigslist.org/about/sites']
 
     def parse(self, response):
+        countries = response.css('.colmask:nth-child(6) a::attr(href), .colmask:nth-child(4) a::attr(href)').getall()
+        print('COUNTRIES LENGTH: ', len(countries))
+
+        for country_link in countries:
+            host = country_link[8:-1]
+            yield scrapy.Request(
+                url=f'{country_link}search/jjj?sort=date&query=python',
+                callback=self.parse_search,
+                headers={
+                    'Host': host,
+                }
+            )
+
+    def parse_search(self, response):
         print(response)
